e0f38600fe6bb2c89854aac0adead9045f006f22
Ashish Parashar <200104016@hbtu.ac.in>
2024-09-06 02:03:55 +0530

    update day 1 work

---
+++ src/components/config/index.ts
@@ -0,0 +1,14 @@
+export const PRODUCT_CATEGORIES=[
+    {
+        label:'Ui Kits',
+        value:"ui_kits" as const,
+        featured:[
+            {
+                name:"Editor picks",
+                href:"#",
+                imageSrc:"/nav/ui-kits/mixed.jpg"
+            },
+            
+        ]
+    }
+];
